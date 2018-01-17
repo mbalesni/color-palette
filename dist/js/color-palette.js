888e30d3a277e27628e03e843fbc490d5dfcfd38
nikita-balesny <nikita.balesny@gmail.com>
2018-01-17 23:51:29 +0200

    HEX switch

--- dist/js/color-palette.js
+++ dist/js/color-palette.js
@@ -1,24 +1,41 @@
-let tiles = document.querySelectorAll('.tile')
-let clipboard = new ClipboardL(tiles)
-clipboard.on('success', (e) => {
-  let clr = ''
-  let elem = e.trigger
-  if (elem.classList.contains('big')) elem = elem.children[0]
-  if (elem.classList.contains('white')) {
-    clr = "#fff"
-  } else if (elem.classList.contains('black')) {
-    clr = '#111'
-  }
-  iziToast.show({
-    title: 'Copied',
-    message: e.text,
-    titleColor: clr,
-    messageColor: clr,
-    backgroundColor: e.text,
-    position: 'topRight',
-    timeout: '1500',
-    progressBar: false,
-    animateInside: false,
-    close: false
+function instColorSaving () {
+  let tiles = document.querySelectorAll('.tile')
+  let clipboard = new ClipboardL(tiles)
+  clipboard.on('success', (e) => {
+    let clr = ''
+    let elem = e.trigger
+    if (elem.classList.contains('big')) elem = elem.children[0]
+    if (elem.classList.contains('white')) {
+      clr = "#fff"
+    } else if (elem.classList.contains('black')) {
+      clr = '#111'
+    }
+    iziToast.show({
+      title: 'Copied',
+      message: e.text,
+      titleColor: clr,
+      messageColor: clr,
+      backgroundColor: e.text,
+      position: 'topCenter',
+      timeout: '1500',
+      progressBar: false,
+      animateInside: false,
+      close: false
+    })
   })
-})
+}
+
+function instHexSwitch () {
+  let palette = document.querySelector('.palette')
+  let btn = document.querySelector('#text-switch')
+  btn.addEventListener('click', () => {
+    if (!btn.checked) {
+      palette.classList.add('no-text')
+    } else {
+      palette.classList.remove('no-text')
+    }
+  })
+}
+
+instColorSaving()
+instHexSwitch()
